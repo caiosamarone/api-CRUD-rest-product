dea0e92709169b2d6b93c38b940fcb4bb9a4c3bf
Caio Mendes <caiosamarone10@hotmail.com>
2020-09-08 18:24:00 -0300

    tornando metodos CRUD em assincronos - async & await

--- src/controllers/product-controllers.js
+++ src/controllers/product-controllers.js
@@ -4,48 +4,60 @@ const ValidatonContract = require('../validators/fluentValidator')
 const ValidationContract = require('../validators/fluentValidator')
 const repository = require('../repositories/product-repository')
 //listar produtos
-exports.get = (req,res,next) => {
-         repository.get()
-        .then(data => {
-            res.status(200).send(data)  //json retornado com a lista de produtos
-        }).catch( e=> {
-            res.status(400).send(e) 
-        }) 
+exports.get = async(req,res,next) => {
+    try {
+        var data = await repository.get()
+        res.status(200).send(data)
+    } catch(e){
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        })
+    }
+    
 }
 
 //listar Produtos pelo slug
-exports.getBySlug = (req,res,next) => {
-        repository.getBySlug(req.params.slug)
-        .then(data => {
+exports.getBySlug = async(req,res,next) => {
+        try {
+            var data = await repository.getBySlug(req.params.slug)
             res.status(200).send(data) 
-        }).catch( e=> {
-            res.status(400).send(e) 
-        }) 
+        }
+        catch(e){
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        })
+    }
 }
 
 //listar produtos pela tag
-exports.getByTag = (req,res,next) => {
-    repository
+exports.getByTag = async(req,res,next) => {
+   try{ 
+        var data = await repository
         .getByTag(req.params.tag) 
-        .then(data => {
-            res.status(200).send(data) 
-        }).catch( e=> {
-            res.status(400).send(e) 
-        }) 
+        res.status(200).send(data)
+        
+    }catch(e){
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        })
+    }
 }
 
 //listar Produtos pelo id
-exports.getById = (req,res,next) => {
-    repository.getById(req.params.id)     
-        .then(data => {
-            res.status(200).send(data)  
-        }).catch( e=> {
-            res.status(400).send(e) 
-        }) 
+exports.getById = async(req,res,next) => {
+    try { 
+    var data = await repository.getById(req.params.id)    
+    res.status(200).send(data)  
+    }catch(e){
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        })
+    }
+        
 }
 
 //create
-exports.post = (req,res,next) => {
+exports.post = async(req,res,next) => {
     let contract = new ValidationContract()
     contract.hasMinLen(req.body.title, 3,'O titulo deve conter no mínimo 3 caracteres')
     contract.hasMinLen(req.body.slug, 3,'O titulo deve conter no mínimo 3 caracteres')
@@ -56,47 +68,45 @@ exports.post = (req,res,next) => {
         res.status(400).send(contract.errors()).end()
         return
     }
-
-        repository.create(req.body)
-            .then(x => {
-                res.status(201).send({
-                    message:'Produto cadastrado'})  
-            }).catch(e => {
-                res.status(400).send(
-                    { message:'Falha ao cadastrar produto', data: e}) 
+        try {
+        var data = await repository.create(req.body)
+        res.status(201).send({
+            message: 'Produto cadastrado com sucesso!'
+        }) 
+        }catch(e){
+            res.status(500).send({
+                message: 'Falha ao processar sua requisição'
             })
-    
-   
+        }        
+      
 
 }  
 
 //put > att produto
-exports.put = (req,res,next) => {
-    repository.update(req.params.id,req.body)
-    .then(x => {
-            res.status(200).send({
-                message:'Produto atualizado'
-            })  
-       }).catch(e => {
-           res.status(400).send({
-                message:'Falha ao atualizar produto', 
-                data: e
-            })
+exports.put = async(req,res,next) => {
+    try {
+    var data = await repository.update(req.params.id,req.body)
+    res.status(200).send({
+        message: 'Produto atualizado com sucesso!'
+    }) 
+    } catch(e){
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
         })
+    }        
 }
 
 
 //delete
-exports.delete = (req,res,next) => {
-        repository.remove(req.body.id)
-            .then(x => {
-                res.status(200).send({
-                    message:'Produto Excluido'
-                })  
-        }).catch(e => {
-            res.status(400).send({
-                    message:'Falha ao excluir produto', 
-                    data: e
-                })
-            })
+exports.delete = async(req,res,next) => {
+    try{
+    var data = await repository.remove(req.body.id)
+    res.status(200).send({
+        message: 'Produto removido com sucesso!'
+    })         
+    }catch(e){
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        })
+    }      
 }
--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -1,41 +1,44 @@
 const mongoose = require('mongoose')
 const Product = mongoose.model('Product')
 
-exports.get = () => {
-    return Product
+exports.get = async() => {
+    const res = await Product    
         .find({
             active:true  //trazer apenas produtos que estao ativos
         }, 'title price slug' )
+        return res
 } 
 
-exports.getBySlug = (slug) =>{
-    return Product
+exports.getBySlug = async(slug) =>{
+    const res = await Product
         .findOne({
         slug: slug,   //traz o slug que tiver setado na requisição
             active:true  
     },     'title description price slug tags')   
+    return res
 }
 
-exports.getById = (id) =>{
-    
-    return Product
+exports.getById = async(id) =>{
+    const res = await Product
         .findById(id)
+    return res
 }
 
-exports.getByTag = (tag) => {
-    return Product.find({
+exports.getByTag = async(tag) => {
+    const res = await Product.find({
         tags: tag,   //traz o slug que tiver setado na requisição
         active:true  
     },  'title description price slug tags')  
+    return res
 }
 
-exports.create = (product) => {
+exports.create = async(product) => {
     var product = new Product(product)
-    return product.save()
+    await product.save()
 }
 
-exports.update = (id, data) => {
-    return Product
+exports.update = async(id, data) => {
+    await Product
         .findByIdAndUpdate(id, {
             $set: {     //setar o que veio da requisição
                 title: data.title,
@@ -46,7 +49,7 @@ exports.update = (id, data) => {
         })
 }
 
-exports.remove = (id) => {
-    return Product
+exports.remove = async(id) => {
+    await Product
     .findByIdAndDelete(id)
 } 
